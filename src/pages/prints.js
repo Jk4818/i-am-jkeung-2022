a0d72ea0db6946812a7bc99eadff08e3055318fd
Jason Keung <work.jk4818@gmail.com>
2022-12-08 17:05:07 +0000

    Feat: added book image

--- src/pages/prints.js
+++ src/pages/prints.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticImage } from "gatsby-plugin-image";
 
 import Layout from '../components/Layout';
 
@@ -6,8 +7,13 @@ import Layout from '../components/Layout';
 function prints(props) {
   return (
     <Layout>
-      <div className='w-screen h-screen bg-white'>
-        asd
+      <div className='w-screen h-full bg-white'>
+
+        <seciton className="w-full h-screen flex items-center justify-center">
+          <div className='w-1/2 '>
+            <StaticImage placeholder='none' layout='constrained' src="../assets/images/prints/frontal.png" alt="Wales" />
+          </div>
+        </seciton>
       </div>
     </Layout>
   );
