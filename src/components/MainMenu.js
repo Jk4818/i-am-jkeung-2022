6832b06062ac7eef6e34c763314464aba0b29eac
Jason Keung <work.jk4818@gmail.com>
2022-12-14 12:10:54 +0000

    Feat: new hamburger animation

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavButton from './NavButton';
 
 function MainMenu(props) {
+
+  const [menuState, setMenuState] = useState(false);
+  
   return (
     <div className={`w-max flex justify-center items-center gap-4 bg-white/70 backdrop-blur-sm p-4 rounded-lg drop-shadow-lg   text-main-black font-archivo font-semibold `}>
       <h1>more</h1>
 
-      <div className=''>
-        <NavButton state={true} /> 
+      <div onClick={() => setMenuState(!menuState)} className=''>
+        <NavButton state={menuState} /> 
       </div>
     </div>
   );
--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -3,15 +3,15 @@ import React from "react";
 function NavButton({ state }) {
 
   return (
-    <div className="w-5 h-5">
-      <div className={`w-full h-full grid grid-rows-3  gap-1 overflow-hidden  transition ease-in-out duration-300`}>
-        <div className={`w-full h-full overflow-hidden ${state && `-rotate-45 translate-y-1.5`} transition ease-in-out duration-300` }>
+    <div className="w-4 h-2.5">
+      <div className={`w-full h-full grid grid-rows-3 gap-0.5 overflow-hidden  transition ease-in-out duration-300`}>
+        <div className={`w-full h-0.5 overflow-hidden ${state && `-rotate-45 translate-y-1`} transition ease-in-out duration-300` }>
           <div className="w-full h-full bg-main-black"></div>
         </div>
-        <div className="w-full h-full overflow-hidden">
-          <div className={`w-full h-full ${state ? `hidden` : `bg-main-black`} transition ease-in-out duration-300`}></div>
+        <div className="w-full h-0.5 overflow-hidden">
+          <div className={`w-full h-full ${state ? `bg-transparent` : `bg-main-black`} transition ease-in-out duration-300`}></div>
         </div>
-        <div className={`w-full h-full overflow-hidden ${state && `rotate-45 -translate-y-1.5`}  transition ease-in-out duration-300` }>
+        <div className={`w-full h-fu1ll overflow-hidden ${state && `rotate-45 -translate-y-1`}  transition ease-in-out duration-300` }>
           <div className="w-full h-full bg-main-black"></div>
         </div>
       </div>
