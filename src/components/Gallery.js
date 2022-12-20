df6f75ff8632ece5f970ffcfea40b76fd4a9f595
Jason Keung <work.jk4818@gmail.com>
2022-12-20 11:05:00 +0000

    Feat: dynamic gallery rendering

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,12 +14,22 @@ function Gallery({ gallaryData }) {
   };
 
   const columnSpan = [
-    'sm:col-span-9',
-    'sm:col-span-6',
-    'sm:col-span-9',
-    'sm:col-span-7',
-    'sm:col-span-9',
-    'sm:col-span-8',
+    [
+      'sm:col-span-9',
+      'sm:col-span-6',
+      'sm:col-span-9',
+      'sm:col-span-7',
+      'sm:col-span-9',
+      'sm:col-span-8',
+    ],
+    [
+      'sm:col-span-8',
+      'sm:col-span-7',
+      'sm:col-span-9',
+      'sm:col-span-9',
+      'sm:col-span-9',
+      'sm:col-span-6',
+    ],
   ]
 
   // function that processes gallery data and returns array of gallery images grouped by every 6
@@ -32,19 +42,19 @@ function Gallery({ gallaryData }) {
     for (let i = 0; i < images.length; i += 6) {
       noGallery.push(images.slice(i, i + 6));
     }
-    return [images, noGallery];
+    return noGallery;
   }
 
-  const [images, noGalleries] = processGalleryData();
+  const galleryData = processGalleryData();
 
 
   return (
     <div className='w-screen h-max'>
-      {noGalleries && noGalleries.map((gallery, index) => {
+      {galleryData && galleryData.map((gallery, galleryIndex) => {
         return (
-          <div className='grid grid-cols-1 sm:grid-cols-24  sm:grid-rows-2  gap-6 lg:gap-4 text-xs font-tinos'>
+          <div key={galleryIndex} className='grid grid-cols-1 sm:grid-cols-24  sm:grid-rows-2  gap-6 lg:gap-4 text-xs font-tinos'>
 
-            {images && images.slice(0, 6).map((image, index) => {
+            {gallery.map((image, index) => {
               console.log(image);
               return (
                 <motion.div
@@ -53,7 +63,7 @@ function Gallery({ gallaryData }) {
                   viewport={{ once: true, amount: 0.6 }}
                   variants={variant}
                   key={index}
-                  className={`${columnSpan[index]} ${index % 6 < 3 && "sm:self-end flex flex-col-reverse"}`}>
+                  className={`${columnSpan[galleryIndex][index]} ${index % 6 < 3 && "sm:self-end flex flex-col-reverse"}`}>
                   <GalleryImage src={image} heading={image.base} subheading={image.base} alt={image.base} reverse={index % 6 < 3 ? true : false} />
                 </motion.div>
               )
