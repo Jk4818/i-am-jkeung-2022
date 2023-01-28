844bb6ac2ed410dcadc68460e07359cd067e99db
Jason Keung <work.jk4818@gmail.com>
2023-01-28 14:53:00 +0000

    Feat: finished animations on latest

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
+
 const headingStyles = {
   marginTop: 0,
   marginBottom: 64,
@@ -27,7 +22,7 @@ const codeStyles = {
 // markup
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
+    <main className="w-screen h-screen bg-white font-archivo">
       <title>Not found</title>
       <h1 style={headingStyles}>Page not found</h1>
       <p style={paragraphStyles}>
@@ -37,13 +32,6 @@ const NotFoundPage = () => {
         </span>{" "}
         we couldn’t find what you were looking for.
         <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
         <br />
         <Link to="/">Go home</Link>.
       </p>
--- src/pages/latest.js
+++ src/pages/latest.js
@@ -1,27 +1,57 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 import { motion } from "framer-motion";
-import Layout from '../components/Layout';
+
+import Logo from '../components/Logo.js';
 
 function Latest(props) {
+
+  const menuItemVariants = {
+    open: {
+      transition: { staggerChildren: 0.07, delayChildren: 0.2 }
+    },
+    closed: {
+      transition: { staggerChildren: 0.05, staggerDirection: -1 }
+    }
+  };
+
+  const itemVariants = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stiffness: 1000, velocity: -100 }
+      }
+    },
+    closed: {
+      y: 20,
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 }
+      }
+    }
+  };
+
   return (
-    <Layout>
-      <div className='w-screen h-screen overflow-none flex justify-center items-center'>
-        <motion.div className='font-archivo text-white w-96 h-1/2 flex flex-col items-center'>
+    <div className='w-screen h-screen overflow-none flex justify-center items-center'>
+      <div><Logo /></div>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 } }} className='font-archivo text-white w-96 h-1/2 flex flex-col items-center'>
 
-          <div className='w-1/2 aspect-square'>
-            <StaticImage className='rounded-full' placeholder='none' layout='constrained' src="../assets/images/tracks/run-to-your-cover.jpg" alt="cover" />
-          </div>
+        <div className='w-1/2 aspect-square'>
+          <StaticImage className='rounded-full' placeholder='none' layout='constrained' src="../assets/images/tracks/run-to-your-cover.jpg" alt="cover" />
+        </div>
 
-          <h1 className='mt-4 text-center'>Run To You <br/> feat. Layla Mehmet, George Chen</h1>
+        <h1 className='mt-4 text-center'>Run To You <br /> feat. Layla Mehmet, George Chen</h1>
 
-          <ul className='w-3/4 h-max mt-10 text-center flex flex-col justify-center items-center gap-4'>
-            <li className='w-full h-max p-2 rounded-lg bg-gray-400'>Spotify</li>
-            <li className='w-full h-max p-2 rounded-lg bg-gray-400'>Youtube</li>
-          </ul>
-        </motion.div>
-      </div>
-    </Layout>
+        <motion.ul initial="closed" animate="open" variants={menuItemVariants} className='w-3/4 h-max mt-10 text-center flex flex-col justify-center items-center gap-4'>
+          <a className='w-full' href="https://open.spotify.com/track/66gyJPSEInZg08XMOKEQ50?si=c3230d157a6941db" target="_blank"><motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Spotify</motion.li>
+          </a>
+          <a className='w-full' href="https://youtu.be/zV6E3BeooKk" target="_blank">
+            <motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Youtube</motion.li>
+          </a>
+        </motion.ul>
+      </motion.div>
+    </div>
   );
 }
 
