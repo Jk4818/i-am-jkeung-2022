813cdc65b76ed6c285eb76a15636376ba2c4c8f6
Jason Keung <work.jk4818@gmail.com>
2023-01-31 13:19:39 +0000

    Feat: refined logo animation

--- src/components/LogoAuto.js
+++ src/components/LogoAuto.js
@@ -8,64 +8,98 @@ function LogoAuto(props) {
 
     const logoVariants = {
         hidden: {
-            opacity: 0,
+            // opacity: 0,
+            x:0
         },
         visible: {
             opacity: 1,
             x: -140,
             transition: {
-                staggerchildren: 2,
-                delayChildren: 1,
+                delay: 0.4,
                 repeat: Infinity,
                 repeatType: 'reverse',
-                repeatDelay: 1,
+                repeatDelay: 3,
+                duration: 0.5
             },
         }
     };
     const logoVariantsHoverIAM = {
         hidden: { 
             y:-20,
-            opacity: 0 
+            opacity: 0,
+            transition: {
+                delay: 0.5,
+            }  
         },
         visible: {
             display: 'inline',
             y: 0,
             opacity: 1,
+            transition: {
+                repeat: Infinity,
+                repeatType: 'reverse',
+                repeatDelay: 3,
+                duration: 0.5
+            }
         }
     };
     const logoVariantsHoverEUNG = {
         hidden: { 
             y:-20,
-            opacity: 0 
+            opacity: 0,
+            transition: {
+                delay: 0,
+            } 
         },
         visible: {
             display: 'inline',
             y: 0,
-            opacity: 1
+            opacity: 1,
+            transition: {
+                delay: 0.1,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                repeatDelay: 3,
+                duration: 0.5
+            }
         }
     };
 
     const logoVariantsJK = {
         hidden: { 
             x:0,
-            opacity: 0 
+            opacity: 1,
         },
         visible: {
             display: 'inline',
             x: 118,
-            opacity: 1
+            opacity: 1,
+            transition: {
+                delay:0.4,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                repeatDelay: 3,
+                duration: 0.5
+            }
         }
     };
 
     const logoVariantsSTUD = {
         hidden: { 
             x:0,
-            opacity: 0 
+            opacity: 1,
         },
         visible: {
             display: 'inline',
             x: 280,
-            opacity: 1
+            opacity: 1,
+            transition: {
+                delay:0.4,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                repeatDelay: 3,
+                duration: 0.5
+            }
         }
     };
 
