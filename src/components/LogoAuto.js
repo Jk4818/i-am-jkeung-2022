dac1c3d02a2e60283b4daab25e439c407865bdd1
Jason Keung <work.jk4818@gmail.com>
2023-01-31 12:12:31 +0000

    Fix: AutoLogo Variant correction

--- src/components/LogoAuto.js
+++ src/components/LogoAuto.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useLocation } from '@reach/router';
-import { Link } from 'gatsby';
 import { motion } from 'framer-motion';
 
 function LogoAuto(props) {
@@ -8,17 +7,26 @@ function LogoAuto(props) {
     const url = useLocation().pathname;
 
     const logoVariants = {
+        hidden: {
+            opacity: 0,
+        },
         visible: {
             opacity: 1,
-            y: 0,
             x: -140,
             transition: {
-                ease: 'easeInOut',
-                delay: 1,
-            }
+                staggerchildren: 2,
+                delayChildren: 1,
+                repeat: Infinity,
+                repeatType: 'reverse',
+                repeatDelay: 1,
+            },
         }
     };
     const logoVariantsHoverIAM = {
+        hidden: { 
+            y:-20,
+            opacity: 0 
+        },
         visible: {
             display: 'inline',
             y: 0,
@@ -26,72 +34,76 @@ function LogoAuto(props) {
         }
     };
     const logoVariantsHoverEUNG = {
+        hidden: { 
+            y:-20,
+            opacity: 0 
+        },
         visible: {
             display: 'inline',
             y: 0,
-            opacity: 1,
-            transition: {
-                ease: 'easeInOut',
-                delay: 1.4,
-            }
+            opacity: 1
         }
     };
 
     const logoVariantsJK = {
+        hidden: { 
+            x:0,
+            opacity: 0 
+        },
         visible: {
             display: 'inline',
             x: 118,
-            opacity: 1,
-            transition: {
-                ease: 'easeInOut',
-                delay: 1,
-            }
+            opacity: 1
         }
     };
 
     const logoVariantsSTUD = {
+        hidden: { 
+            x:0,
+            opacity: 0 
+        },
         visible: {
             display: 'inline',
             x: 280,
-            opacity: 1,
-            transition: {
-                ease: 'easeInOut',
-                delay: 1,
-            }
+            opacity: 1
         }
     };
 
 
     return (
         <motion.div
-            initial={{ x: 0 }}
-            animate={"visible"}
+            initial="hidden"
+            animate="visible"
             variants={logoVariants}
+
             className={`relative w-72 h-20 text-7xl font-archivo font-semibold text-main-gray-darker transition-colors  text-center `}>
 
-            <Link to='/'>
-                <motion.span 
-                initial={{ opacity: 0, y: -20 }} 
+            <motion.span
                 variants={logoVariantsHoverIAM}
-                transition={{
-                    ease: 'easeInOut',
-                    delay: 1.3,
-                    repeat: Infinity,
-                    repeattype: 'mirror',
-                    duration: 1.5,
-                }} 
-                className="absolute left-0 top-0" >iam</motion.span>
+                className="absolute left-0 top-0" >
+                iam
+            </motion.span>
 
-                <motion.span exit={{ transition: { delay: 0.3 } }} variants={logoVariantsJK} className="absolute left-0 top-0 ">jk</motion.span>
+            <motion.span
+                variants={logoVariantsJK}
+                className="absolute left-0 top-0 ">
+                jk
+            </motion.span>
 
-                <motion.span initial={{ opacity: 0, y: -20 }} variants={logoVariantsHoverEUNG} className="absolute left-[10.65rem] top-0">eung</motion.span>
+            <motion.span
+                variants={logoVariantsHoverEUNG}
+                className="absolute left-[10.65rem] top-0">
+                eung
+            </motion.span>
 
-                <motion.span exit={{ transition: { delay: 0.3 } }} variants={logoVariantsSTUD} className="absolute left-[3.25rem] top-0">
-                    .
-                    <span className='logo-animate'>studio</span>
-                </motion.span>
+            <motion.span
+                variants={logoVariantsSTUD}
+                className="absolute left-[3.25rem] top-0">
+                .
+                <span className='logo-animate'>studio</span>
+            </motion.span>
 
-            </Link>
+
 
         </motion.div>
     );
