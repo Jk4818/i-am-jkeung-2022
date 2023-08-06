cc9a9f9a0ff0bfeea22c1f750fc03d99f70e988a
Jason Keung <work.jk4818@gmail.com>
2023-08-06 20:49:27 +0100

    Fix: repaired all warnings

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -5,14 +5,14 @@ function MenuButton({menuState, setMenuState}) {
 
   
   return (
-    <div onClick={() => setMenuState(!menuState)} className={`relative w-max flex justify-center items-center gap-3 ${!menuState ? `bg-white/70 hover:bg-white` : `bg-main-gray-dark`} backdrop-blur-sm p-1 px-3 rounded-lg shadow-main font-semibold font-archivo cursor-pointer transition-colors`}>
+    <button onClick={() => setMenuState(!menuState)} className={`relative w-max flex justify-center items-center gap-3 ${!menuState ? `bg-white/70 hover:bg-white` : `bg-main-gray-dark`} backdrop-blur-sm p-1 px-3 rounded-lg shadow-main font-semibold font-archivo cursor-pointer transition-colors`}>
 
       <h1 className={`${menuState ? `text-white` : `text-main-black`}`}>more</h1>
 
       <div className='h-full'>
         <MenuBurger state={menuState} /> 
       </div>
-    </div>
+    </button>
   );
 }
 
