acf2f7bf600e0f71ddb0bf1816da85588bddd35b
Jason Keung <work.jk4818@gmail.com>
2023-11-16 15:54:53 +0000

    New release

--- src/pages/latest.js
+++ src/pages/latest.js
@@ -38,18 +38,20 @@ function Latest(props) {
       <div className='w-screen h-screen overflow-none flex justify-center items-center'>
         <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 } }} className='font-archivo text-white w-96 h-1/2 flex flex-col items-center'>
           <div className='w-1/2 aspect-square'>
-            <StaticImage className='rounded-full' placeholder='none' layout='constrained' src="../assets/images/tracks/the_road_not_taken.jpg" alt="cover" />
+            <StaticImage className='rounded-full' placeholder='none' layout='constrained' src="../assets/images/tracks/seven_cover.jpeg" alt="cover" />
           </div>
-          <h1 className='mt-4 text-center'>The Road Not Taken - EP</h1>
+          <h1 className='text-lg mt-4 text-center'>Seven - Cover</h1>
+          <h1 className='text-sm mt-1 text-center'>Amatta Mirandari, George Chen</h1>
           <motion.ul initial="closed" animate="open" variants={menuItemVariants} className='w-3/4 h-max mt-10 text-center flex flex-col justify-center items-center gap-4'>
-            <a className='w-full' href="https://open.spotify.com/album/36lKVvGQdFeHoOUTw8G1pz?si=_CkLr-JqS7Ch_MvVyvlOHA" rel="noreferrer" target="_blank"><motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Spotify</motion.li>
-            </a>
-            <a className='w-full' href="https://music.apple.com/us/album/the-road-not-taken-ep/1706231503" rel="noreferrer" target="_blank">
-              <motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Apple Music</motion.li>
-            </a>
-            <a className='w-full' href="https://youtu.be/FIWVuUGtO38?si=N9MHoEPOaElVc9b9" rel="noreferrer" target="_blank">
+
+            <a className='w-full' href="https://youtu.be/Q1i13wkOEnw?si=5eDvurOvQf9FbqBH" rel="noreferrer" target="_blank">
               <motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Youtube</motion.li>
             </a>
+            <a className='w-full' href="https://open.spotify.com/track/5EQT4XOR6yMLwfSOglnLih?si=YXMztqGJSfSnCa0NTPkwqA" rel="noreferrer" target="_blank"><motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Spotify</motion.li>
+            </a>
+            {/* <a className='w-full' href="https://music.apple.com/us/album/the-road-not-taken-ep/1706231503" rel="noreferrer" target="_blank">
+              <motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Apple Music</motion.li>
+            </a> */}
           </motion.ul>
         </motion.div>
       </div>
