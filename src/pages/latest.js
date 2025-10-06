7b76fb96928fe85afcf391dbb62936a4c12c84da
Jason Keung <work.jk4818@gmail.com>
2025-10-06 14:54:05 +0100

    Feat: sylva MV link added

--- src/pages/latest.js
+++ src/pages/latest.js
@@ -43,9 +43,9 @@ function Latest(props) {
           <h1 className='text-sm mt-1 text-center'>Jason Keung</h1>
           <motion.ul initial="closed" animate="open" variants={menuItemVariants} className='w-3/4 h-max mt-10 text-center flex flex-col justify-center items-center gap-4'>
 
-            {/* <a className='w-full' href="https://youtu.be/96bhg2eNMSM" rel="noreferrer" target="_blank">
+            <a className='w-full' href="https://youtu.be/9lkpSN5BcR8" rel="noreferrer" target="_blank">
               <motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Youtube</motion.li>
-            </a> */}
+            </a>
             <a className='w-full' href="https://open.spotify.com/track/6TrCufjG4lIOG585dRDri1?si=d1eed47c7d064782" rel="noreferrer" target="_blank"><motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Spotify</motion.li>
             </a>
             <a className='w-full' href="https://music.apple.com/us/album/sylva-single/1842959447" rel="noreferrer" target="_blank"><motion.li initial="closed" animate="open" variants={itemVariants} className='w-full h-max p-2 rounded-lg bg-gray-400 hover:bg-gray-500 transition-all'>Apple Music</motion.li>
